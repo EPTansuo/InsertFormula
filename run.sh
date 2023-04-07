77ade4aff21db63ccee60d3cfbd890ec4a8cfa70
EPTansuo <eptansuo@163.com>
2023-04-07 11:24:14 +0800

    第一个可用版本

--- run.sh
+++ run.sh
@@ -3,12 +3,19 @@
 #
 workdir="/home/han/Disk/Document/PROJECT/Python/InsertFormula"
 source "$workdir/myenv/bin/activate"
-cd $wordkir
 
-echo "Error: you have to config the app_id and app_key!"
-exit 1 
-#app_id="your app id"
-#app_key="your app key"
+#echo "Error: you have to config the app_id and app_key!"
+#exit 1
+#APP_ID="your app id"
+#APP_KEY="your app key"
+
+
+cd $workdir
+
+if [ ! -d "out_files" ]; then
+	mkdir "out_files"
+fi
+
 
 python main.py
 
